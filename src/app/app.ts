743cb7b50fe69b62747da134ee496d8025d818ae
[ayobkina-blip] <[ayobkina@gmail.com]>
2025-12-17 14:36:37 +0100

    Version inicial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroItem } from './components/hero-item/hero-item';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, HeroItem],
+  template: "<app-hero-item/>",
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('AngularProyecto');
